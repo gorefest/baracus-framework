ed8e38ea21a9f23eca872c7b0c7c7d0855822600
gorefest <noerdertuitje@gmail.com>
2013-11-15 15:57:59 +0100

    noerder-tuitje; validation for managed activities

--- src/main/java/net/mantucon/baracus/context/ManagedActivity.java
+++ src/main/java/net/mantucon/baracus/context/ManagedActivity.java
@@ -27,4 +27,38 @@ public class ManagedActivity extends Activity{
         super.onDestroy();
     }
 
+
+    /**
+     * enables the validation of the view onFocusChanged
+     * If you want Your View to
+     * be able to receive a validation callback - e.g. in order to manage the
+     * visibility of an OK-Button or sth. - Your View must implement
+     * the @see ValidatableView interface in order to receive a validation
+     * notification.
+     *
+     * Notice, You must set the underlying view instance to enable this feature!
+     *
+     */
+    public void enableFocusChangeBasedValidation() {
+        BaracusApplicationContext.registerValidationListener(underlyingView);
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+
+    /**
+     * perform a validation on all validateable fields, map the errors the the view
+     *
+     * @return a boolean indicating whether the view is valid
+     */
+    public boolean validate() {
+        BaracusApplicationContext.resetErrors(underlyingView);
+        BaracusApplicationContext.validateView(underlyingView);
+        BaracusApplicationContext.applyErrorsOnView(underlyingView);
+        return !BaracusApplicationContext.viewHasErrors(underlyingView);
+    }
+
 }
