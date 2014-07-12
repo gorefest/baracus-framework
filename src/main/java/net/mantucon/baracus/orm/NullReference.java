bee61d72e381d2ff433805b9618d94848e86574d
gorefest <noerdertuitje@gmail.com>
2014-07-12 10:06:15 +0200

    - added NullReference in ORM to avoid NPE handling on nullified References

---
+++ src/main/java/net/mantucon/baracus/orm/NullReference.java
@@ -0,0 +1,21 @@
+package net.mantucon.baracus.orm;
+
+/**
+ * Crash-avoiding helper. Use this Reference to instantiate any
+ * Reference. Instead of dealing NPE due to null references You will simply
+ * get a Null value. Makes handling a little bit easier.
+ * <p/>
+ * Created by marcus on 12.07.14.
+ */
+public final class NullReference<T extends AbstractModelBase> implements Reference<T> {
+
+    @Override
+    public final T getObject() {
+        return null;
+    }
+
+    @Override
+    public final Long getObjectRefId() {
+        return null;
+    }
+}
